99bc2d5c5b4eb97a814d06398edb418bba503c6d
juanT-27 <jtsoftdeveloper@gmail.com>
2024-06-22 10:27:22 -0500

    more design

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const preloader= document.querySelector(`[data-preloader]`);
 const navBtn= document.querySelector(`.navBtn`);
-const navBar= document.querySelector(".nav")
-const toggleVisibility = (e)=>{
-    e.classList.toggle("visibility")
-    
+const navBar= document.querySelector(".nav");
+const navLink= document.querySelectorAll(".navLink")
+// function to toggle a single class of an element
+const toggleVisibility = (elem, classText)=>{
+    elem.classList.toggle(classText)
 }
+
+// domloaded
 document.addEventListener("DOMContentLoaded", (e)=>{
-    navBtn.addEventListener("click", e=> toggleVisibility(navBar))
-})
+    navBtn.addEventListener("click", ()=> toggleVisibility(navBar, "visibility"))
+    })
 
 window.addEventListener("load", (e)=>{
     setTimeout(() => {
